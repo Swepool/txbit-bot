5c5e78e541d40914bede40010b8399e9a03f976a
Lukas <Lukas.nyberg@gmail.com>
2022-11-26 23:45:45 +0100

    Started on exbitron

---
+++ exbitron/index.js
@@ -0,0 +1,30 @@
+// Kryptokrona ©2022
+const crypto = require("crypto");
+const {fetch} = require('cross-fetch')
+
+const URL = 'https://www.exbitron.com/api/v2/peatio'
+
+function API(key , secret ) {
+
+    const apiKey = key
+    const apiSecret = secret
+    const nonce = Date.now()
+
+    this.getBalances = async function () {
+        const endpoint = URL + `/account/balances`
+        const req = await fetch(endpoint, {
+            headers: {
+                "X-Auth-Apikey": apiKey,
+                "X-Auth-Nonce": nonce,
+                "X-Auth-Signature": sign(apiSecret, (nonce + apiKey))
+            }
+        })
+        return await req.json()
+    }
+}
+
+const sign = (apiSecret, data) => {
+    return crypto.createHmac("sha256",apiSecret).update(data).digest('hex')
+}
+
+exports.API = API
--- main.js
+++ main.js
@@ -1,24 +1,29 @@
 const clc = require("cli-color");
 const TxBit = require('./txbit');
+const Exbitron = require('./exbitron/index')
 
 require('dotenv').config()
 
 // NEVER SHARE YOUR KEYS
-const KEY = process.env.API_KEY
-const SECRET = process.env.API_SECRET
+const TXBIT_KEY = process.env.TXBIT_API_KEY
+const TXBIT_SECRET = process.env.TXBIT_API_SECRET
+const EXBITRON_KEY = process.env.EXBITRON_API_KEY
+const EXBITRON_SECRET = process.env.EXBITRON_API_SECRET
 
-const publicApi = new TxBit.Public()
-const accountApi = new TxBit.Account(KEY, SECRET)
-const marketApi = new TxBit.Market(KEY, SECRET)
+const txbitPublic = new TxBit.Public()
+const txbitAccount = new TxBit.Account(TXBIT_KEY, TXBIT_SECRET)
+const txbitMarket = new TxBit.Market(TXBIT_KEY, TXBIT_SECRET)
+const exbitron = new Exbitron.API(EXBITRON_KEY, EXBITRON_SECRET)
 
 console.log(clc.green('BOT STARTED 🥳'))
 
 const main = async () => {
 
     //JUST A TEST, YOU CAN REMOVE THIS
-    console.log(await accountApi.getBalance('BTC'))
+    //console.log(await accountApi.getBalance('BTC'))
 
     //NOW, BUILD SOMETHING COOL BELOW 😎🤙
+    console.log(await exbitron.getBalances())
 }
 
 main()
